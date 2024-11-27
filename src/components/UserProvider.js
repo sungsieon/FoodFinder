4fc2b1304b8d161a6ed5b02c1226df0269ef3f29
sungsieon <mari9908@naver.com>
2024-11-28 05:16:08 +0900

    login

--- src/components/UserProvider.js
+++ src/components/UserProvider.js
@@ -8,12 +8,12 @@ export function useUserContext() {
 
 export default function UserProvider({ children }) {
   const [userData, setUserData] = useState(() => {
-    // 로컬 스토리지에서 userData 가져오기
+   
     const savedUserData = localStorage.getItem('userData');
     return savedUserData ? JSON.parse(savedUserData) : null;
   });
 
-  // userData가 변경될 때 로컬 스토리지에 저장
+  
   useEffect(() => {
     localStorage.setItem('userData', JSON.stringify(userData));
   }, [userData]);
