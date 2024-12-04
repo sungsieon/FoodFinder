ab776f65f47978f305cf1afb9396ae12df43445b
sungsieon <mari9908@naver.com>
2024-12-05 08:31:04 +0900

    search fix

--- src/components/KakaoMap.js
+++ src/components/KakaoMap.js
@@ -92,10 +92,9 @@ export default function KakaoMap() {
         });
 
         var infowindow = new kakao.maps.InfoWindow({
-          content: `<div className="infodiv" style="padding:5px; border:3px solid orange; background-color: white;">
+          content: `<div className="infodiv" style="padding:5px; width:15rem;  border:3px solid orange; background-color: white;">
     ${el.TITLE}<br><br>
     주소 : ${el.ADDR1}<br>
-    운영 시간 : ${el.USAGE_DAY_WEEK_AND_TIME}<br>
     메뉴 : ${el.TITLE}<br>
     
     </div>`,
@@ -162,12 +161,12 @@ export default function KakaoMap() {
       <Asidebar data={data} mapData={mapData} toggleList={clickList} divRef={divRef}/>
       <div className="searchBox">
       <input
-      className="search"
+      className={clickList ? "search" : "search2"}
       placeholder="맛집 이름 검색"
       onChange={handleInput}
       onKeyDown={keyEnter}
       />
-      <svg style={{cursor:"pointer"}} onClick={inputCunnection} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>   
+      <svg className={clickList ? "searchBoxSvg" : "searchBoxSvg2"} style={{cursor:"pointer"}} onClick={inputCunnection} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>   
       </div>
       <div className={randomBtn ? "show" : "randomFood"}>
         <span onClick={closeBtn} className="closeBtn">
